11fc37ab4788a852ee8d918054eaeac8e4b5cd3c
sajjadkareem <ggh_ggh.1998@yahoo.com>
2022-04-11 08:03:51 +0300

    add datatable

---
+++ datatables/datatable.php
@@ -0,0 +1,20 @@
+  <!-- DataTables -->
+  <!--jquery-->
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+
+   <script src="<?php echo base_url() . 'template/'; ?>plugins/datatables/jquery.dataTables.js"></script>
+  <script src="<?php echo base_url() . 'template/'; ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.bootstrap.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.buttons.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.bootstrap.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/jszip.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/pdfmake.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/vfs_fonts.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.html5.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.print.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.fixedHeader.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.keyTable.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.responsive.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/responsive.bootstrap.min.js"></script>
+  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.scroller.min.js"></script> 
