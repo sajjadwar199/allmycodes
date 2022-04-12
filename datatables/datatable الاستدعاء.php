161b432a78d8f751145ea214584b422cbc7ae3ba
sajjadkareem <ggh_ggh.1998@yahoo.com>
2022-04-12 23:06:52 +0300

    update datatables files

--- datatables/datatable الاستدعاء.php
+++ datatables/datatable الاستدعاء.php
@@ -1,20 +1,34 @@
-  <!-- DataTables -->
-  <!--jquery-->
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+ <!--datatables css -->
+ <link type="text/css" href="<?php echo base_url() . 'template/'; ?>plugins/datatables/dataTables.bootstrap4.css">
+ 
+ <link type="text/css" href="<?php echo base_url() . 'template/'; ?>plugins/datatables/fixedHeader.bootstrap.min.css
+">
+<link type="text/css" href="<?php echo base_url() . 'template/'; ?>plugins/datatables/buttons.bootstrap.min.css
+">
+<link type="text/css" href="<?php echo base_url() . 'template/'; ?>plugins/datatables/jquery.dataTables.min.css">
+<link type="text/css" href="<?php echo base_url() . 'template/'; ?>plugins/datatables/responsive.bootstrap.min.css">
+<link type="text/css" href="<?php echo base_url() . 'template/'; ?>plugins/datatables/scroller.bootstrap.min.css">
 
-   <script src="<?php echo base_url() . 'template/'; ?>plugins/datatables/jquery.dataTables.js"></script>
-  <script src="<?php echo base_url() . 'template/'; ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/jquery.dataTables.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.bootstrap.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.buttons.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.bootstrap.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/jszip.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/pdfmake.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/vfs_fonts.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.html5.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.print.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.fixedHeader.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.keyTable.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.responsive.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/responsive.bootstrap.min.js"></script>
-  <script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.scroller.min.js"></script> 
+</script>
+<!--jquery-->
+<script src="<?php  echo  base_url().'template/';?>vendor/jquery/jquery.min.js"></script>
+
+<!--datatables js -->
+<script src=" <?php echo   base_url() . 'template/'; ?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src=" <?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.bootstrap4.js"></script>
+
+<script src=" <?php echo   base_url() . 'template/'; ?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.bootstrap.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.buttons.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.bootstrap.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/jszip.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/pdfmake.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/vfs_fonts.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.html5.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/buttons.print.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.fixedHeader.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.keyTable.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.responsive.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/responsive.bootstrap.min.js"></script>
+<script src="<?php echo   base_url() . 'template/'; ?>plugins/datatables/dataTables.scroller.min.js"></script> 
+<script src="<?php echo base_url() . 'template/'; ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
